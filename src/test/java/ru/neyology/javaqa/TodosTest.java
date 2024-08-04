1b48a12028d0387a7e38a96fe6705fdb8ff12f78
Aleksandr Stupnikov <a-a-stupnikov@yandex-team.ru>
2024-08-04 18:12:30 +0300

    hw13

--- src/test/java/ru/neyology/javaqa/TodosTest.java
+++ src/test/java/ru/neyology/javaqa/TodosTest.java
@@ -128,6 +128,30 @@ public class TodosTest {
         Task[] actual = todos.search("Тест");
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void shouldSearchпss() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("  ");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
     /////////////////////////////////////////////////////////////////////////////////////////////////////
 
